625efa710df6f05740e17c158b50858fcb07bfef
Gabriel Zerbe <gabriel.zerbe@gob.de>
2023-11-27 10:08:44 +0100

    Removed unused Project

--- Simple-Neural-Network/Activation/IActivationFunction.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Simple_Neural_Network.Activation;
-/// <summary>
-/// Interface for activation functions.
-/// </summary>
-public interface IActivationFunction
-{
-    double CalculateOutput(double input);
-}
